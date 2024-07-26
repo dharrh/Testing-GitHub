327f96a5a8c8c0037054454e4754f5458dd1694b
Rhea Dhar <dharrh101@gmail.com>
2024-07-26 11:41:09 -0400

    Initial Commit

---
+++ Testing GitHub/Testing_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testing_GitHubApp.swift
+//  Testing GitHub
+//
+//  Created by Rhea Dhar on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Testing_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
